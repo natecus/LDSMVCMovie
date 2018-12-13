df61a969edb89c3480dc308484a8c458932d991e
Nate <Nate@>
2018-12-13 09:49:45 -0800

    Made the entire project

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                if (context.Movie.Any())
+                {
+                    return;
+                }
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Down and Derby",
+                        ReleaseDate = DateTime.Parse("12-12-1212"),
+                        Genre = "Comedy",
+                        Rating ="PG",
+                        Price = 7.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("12-12-1212"),
+                        Genre = "Drama",
+                        Rating = "PG",
+                        Price = 14.99M
+                    },
+                    new Movie
+                    {
+                        Title = "President Russell M. Nelson: Brilliant Mind, Gentle Heart",
+                        ReleaseDate = DateTime.Parse("12-12-1212"),
+                        Genre = "Documentary",
+                        Rating = "PG",
+                        Price = 12.99M
+                    },
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("12-12-1212"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    },
+                    new Movie
+                    {
+                        Title = "Family Night with John Bytheway",
+                        ReleaseDate = DateTime.Parse("12-12-1212"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    }
+
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MvcMovie/Program.cs
+++ MvcMovie/Program.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Models;
+using MvcMovie;
+using Microsoft.EntityFrameworkCore;
+
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +19,24 @@ namespace MvcMovie
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+
+                    var context = services.GetRequiredService<MvcMovieContext>();
+                    context.Database.Migrate();
+                    SeedData.Initialize(services);                   
+                }
+                catch(Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured while seeding data");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
